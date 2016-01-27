3750fe4a62bb27337da2d2b98c038fa516fa869f
khaletran <tranlekha1979@gmail.com>
2016-01-28 02:06:50 +0700

    First commit

---
+++ dist/README.md
@@ -0,0 +1,47 @@
+# Project 4 Website Optimization
+Structure of the directory:
+- `./src` source code
+- `./dist` production code
+- `./gulpfile.js` gulp file
+## Part 1: Optimize PageSpeed Insights score for index.html
+1. How to run app:
+    - checkout `./dist/index.html`
+    - run local server on that directory
+    - download and install *ngrok*
+    - copy the public URL that *ngrok* makes and paste to PageSpeed Insights
+2. How to optimize website:
+    - HTML: minify html, Web Font Loader, media query ,inline CSS
+    - CSS: minify CSS, inline CSS
+    - JavaScript: minify JavaScript, async JS
+3. How to build tools to optimize website:
+    - Using gulp on `./gulpfile.js`
+    - Install *npm* and related *gulp* packages on directory `./`
+    ```sh
+    $ npm install --save-dev gulp-uglify
+    $ npm install --save-dev gulp-minify-css
+    $ npm install --save-dev gulp-htmlmin
+    $ npm install --save-dev gulp-imagemin
+    $ npm install --save-dev gulp-pngquant
+    $ npm install --save-dev gulp-inline-css
+    $ npm install --save-dev gulp-image-resize
+
+    ```
+
+    - On command line, navigate to directory, type `gulp`
+    ```sh
+    $ gulp
+    ```
+    - This command will automatically update changes, executes the changes and be watched on terminal screen.
+    - The production code on this case is in `./dist` directory
+## Part 2: Optimize Frames per Second in pizza.html
+1. How to run app:
+    - checkout and open `./dist/views/pizza.html`on browser
+    - open Dev Tool and record to check on TimeLine
+2. How to optimize Frames per Second:
+    - JavaScript: `./dist/views/js/main.js`
+        - fine tune `for` loop on function `changePizzaSizes()`
+        - fine tune `for` loop on function `updatePostions()`
+        - replace `querySelectorAll` by `getElementsByClassName`
+        - reduce the number of pizza when the page loaded and scroll
+    - CSS:`./dist/views/css/style.css`
+        - add `will-change: transform` to class `.mover`
---
+++ gulpfile.js
@@ -0,0 +1,109 @@
+var gulp = require('gulp'),
+	uglify = require('gulp-uglify'), //minify JS
+	minifyCSS = require('gulp-minify-css'),//minify CSS
+	htmlmin = require('gulp-htmlmin'); //minify HTML
+	imagemin = require('gulp-imagemin'), //optimize images
+	pngquant = require('imagemin-pngquant'); //optimize .png
+	inlineCSS = require('gulp-inline-css'); // inline CSS
+	imageResize = require('gulp-image-resize');//resize image
+	browserSync = require('browser-sync');
+
+var paths = {
+	scripts:['src/js/*.js'],
+	styles: ['src/css/*.css'],
+	images: ['src/img/*', 'src/views/images/*'],
+	contents: ['src/index.html']
+};
+
+// Minify JS files
+gulp.task('scripts', function() {
+	 return gulp.src(paths.scripts)
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/js/'));
+});
+
+// Minify CSS files
+gulp.task('styles', function() {
+	return gulp.src(paths.styles)
+		.pipe(minifyCSS())
+		.pipe(gulp.dest('dist/css/'));
+});
+
+// Inline CSS
+gulp.task('inlinecss', function() {
+	return gulp.src(paths.contents)
+		.pipe(inlineCSS())
+		.pipe(gulp.dest('dist/'));
+});
+
+// Optimize images
+gulp.task('images1', function() {
+	return gulp.src(paths.images[0])
+		.pipe(imagemin( {
+			progressive: true,
+			use: [pngquant()]
+		}))
+		.pipe(gulp.dest('dist/img/'));
+
+	});
+
+gulp.task('images2', function() {
+	return gulp.src(paths.images[1])
+		.pipe(imagemin( {
+			progressive: true,
+			use: [pngquant()]
+		}))
+		.pipe(imageResize({
+			width: 115,
+			height: 55,
+			crop: true,
+			upscale: false
+		}))
+		.pipe(gulp.dest('dist/views/images/'));
+
+	});
+
+// Minify HTML files
+gulp.task('contents', ['scripts', 'styles', 'inlinecss', 'images1', 'images2'],function() {
+	return gulp.src('dist/index.html')
+		.pipe(htmlmin({collapseWhitespace: true, removeComments: true}))
+		.pipe(gulp.dest('dist/'));
+});
+
+// Execute the gulp
+//gulp.task('default', ['scripts', 'styles', 'inlinecss', 'images1', 'images2', 'contents']);
+
+
+gulp.task('contents-watch', ['contents', 'scripts', 'styles', 'inlinecss', 'images1', 'images2'], browserSync.reload);
+
+gulp.task('scripts-watch', ['contents', 'scripts', 'styles', 'inlinecss', 'images1', 'images2'], browserSync.reload);
+gulp.task('styles-watch', ['contents', 'scripts', 'styles', 'inlinecss', 'images1', 'images2'], browserSync.reload);
+gulp.task('inlinecss-watch', ['contents', 'scripts', 'styles', 'inlinecss', 'images1', 'images2'], browserSync.reload);
+gulp.task('images1-watch', ['contents', 'scripts', 'styles', 'inlinecss', 'images1', 'images2'], browserSync.reload);
+gulp.task('image2-watch', ['contents', 'scripts', 'styles', 'inlinecss', 'images1', 'images2'], browserSync.reload);
+
+gulp.task('browse', function() {
+	browserSync({
+		port: 8080,
+		server: {
+			baseDir: "./dist"
+		}
+	});
+
+	gulp.watch(paths.contents, ['contents-watch']);
+
+	gulp.watch(paths.scripts, ['scipts-watch']);
+	gulp.watch(paths.styles, ['styles-watch']);
+	gulp.watch(paths.contents, ['inlinecss-watch']);
+	gulp.watch(paths.images[0], ['images1-watch']);
+	gulp.watch(paths.images[1], ['images2-watch']);
+});
+
+
+gulp.task('default', ['scripts', 'styles', 'inlinecss', 'images1', 'images2', 'contents','browse']);
+
+
+
+
+
+
